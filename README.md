baa3fe4a6f052bf9761bccbb5753ef26d90205c6
jahir <supersimple.jahir@gmail.com>
2024-09-04 12:37:00 +0600

    [Done] all service page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Live link Home page: 
-Live link src page: 
+Live link Home page: https://qtecsolution.github.io/legal-71/
+Live link src page: https://qtecsolution.github.io/legal-71/src/about-us.html
 
 #Tailwind run code:
 npx tailwindcss -i ./src/assets/css/input.css -o ./src/assets/css/style.css --watch
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,27 +5,3 @@ document.addEventListener("DOMContentLoaded", function (e) {
     NiceSelect.bind(select);
   });
 });
-
-//select2
-$(document).ready(function () {
-  $(".service_select").select2();
-});
-
-$(function () {
-  $('input[name="datefilter"]').daterangepicker({
-    autoUpdateInput: false,
-    locale: {
-      cancelLabel: "Clear",
-    },
-  });
-
-  $('input[name="datefilter"]').on("apply.daterangepicker", function (ev, picker) {
-    $(this).val(
-      picker.startDate.format("MM/DD/YYYY") + " - " + picker.endDate.format("MM/DD/YYYY")
-    );
-  });
-
-  $('input[name="datefilter"]').on("cancel.daterangepicker", function (ev, picker) {
-    $(this).val("");
-  });
-});
