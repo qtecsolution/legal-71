40f44a93ee36a1a217f0dfba2e5e8632dda27116
jahir <supersimple.jahir@gmail.com>
2024-09-05 14:06:47 +0600

    [Done] testimonial section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,16 @@
 // nice selector 2
-document.addEventListener("DOMContentLoaded", function (e) {
-  var els = document.querySelectorAll(".selectize");
-  els.forEach(function (select) {
-    NiceSelect.bind(select);
-  });
+// document.addEventListener("DOMContentLoaded", function (e) {
+//   var els = document.querySelectorAll(".selectize");
+//   els.forEach(function (select) {
+//     NiceSelect.bind(select);
+//   });
+// });
+
+// testimonialSwiper
+const swiper = new Swiper(".testimonialSwiper", {
+  loop: true,
+  navigation: {
+    nextEl: "#testimonialNextButton",
+    prevEl: "#testimonialPrevButton",
+  },
 });
