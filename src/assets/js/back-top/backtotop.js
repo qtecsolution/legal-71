55405c927f4231b2ee429f2cb0242c6f3c651ef4
jahir <supersimple.jahir@gmail.com>
2024-09-05 17:36:11 +0600

    [Done] some modification all page

---
+++ src/assets/js/back-top/backtotop.js
@@ -0,0 +1,18 @@
+up({
+  // bottom position
+  bottom: "1.5rem",
+  // right position
+  right: ".75rem",
+  // width
+  width: "3rem",
+  // height
+  height: "3rem",
+  // background color
+  bg: "#F05A2B",
+  // custom icon
+  src: "assets/image/cursor/chevron-up-solid.svg",
+  // distance from the top to show the back to top button
+  whenShow: 400,
+  // circular button?
+  circle: true,
+});
