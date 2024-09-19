633cca4e4dad0d89be29b9b6f89d29b4e91cb0ce
jahir <supersimple.jahir@gmail.com>
2024-09-19 13:04:36 +0600

    [Done] daisyUI js hide

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,3 @@
-// nice selector 2
-// document.addEventListener("DOMContentLoaded", function (e) {
-//   var els = document.querySelectorAll(".selectize");
-//   els.forEach(function (select) {
-//     NiceSelect.bind(select);
-//   });
-// });
-
 // testimonialSwiper
 const swiper = new Swiper(".testimonialSwiper", {
   loop: true,
