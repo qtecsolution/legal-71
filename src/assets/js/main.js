bb129f185f05e6faeec5a1481b5830fc97b1ae4d
Md Jahirul Islam <supersimple.jahir@gmail.com>
2024-11-02 00:33:10 +0600

    [Done] add swiper js ourTeamMembers section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,3 +6,27 @@ const swiper = new Swiper(".testimonialSwiper", {
     prevEl: "#testimonialPrevButton",
   },
 });
+
+var ourTeamMembers = new Swiper(".ourTeamMembers", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  breakpoints: {
+    550: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
